40d3f048ca2f97a110e4c526edb3711a0b650394
Brian Holt <bdholt1@gmail.com>
2021-06-17 17:23:24 +0100

    change altitude controller to subscribe to euler angles instead of imu message

--- altitude_controller/src/altitude_controller_node.cpp
+++ altitude_controller/src/altitude_controller_node.cpp
@@ -3,11 +3,8 @@
 #include <memory>
 #include <mutex>
 
-#include <Eigen/Geometry>
-
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float64.hpp"
-#include "sensor_msgs/msg/imu.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -21,9 +18,11 @@ public:
     publisher_ = this->create_publisher<std_msgs::msg::Float64>("output", 10);
 
     control_sub_ = this->create_subscription<std_msgs::msg::Float64>(
-        "input", 10, std::bind(&AltitudeController::control_callback, this, _1));
-    imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
-        "imu", 10, std::bind(&AltitudeController::imu_callback, this, _1));
+        "control_input", 10, std::bind(&AltitudeController::control_callback, this, _1));
+    roll_sub_ = this->create_subscription<std_msgs::msg::Float64>(
+        "roll", 10, std::bind(&AltitudeController::roll_callback, this, _1));
+    pitch_sub_ = this->create_subscription<std_msgs::msg::Float64>(
+        "pitch", 10, std::bind(&AltitudeController::pitch_callback, this, _1));
 
     this->declare_parameter<double>("freq", 1000.0);
     this->declare_parameter<double>("mass", 1.0);
@@ -56,14 +55,16 @@ private:
     control_ = msg->data;
   }
 
-  void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+  void roll_callback(const std_msgs::msg::Float64::SharedPtr msg)
   {
-    Eigen::Quaterniond q(msg->orientation.w, msg->orientation.x, msg->orientation.y, msg->orientation.z);
-    auto euler = q.toRotationMatrix().eulerAngles(0, 1, 2);
-
     std::lock_guard<std::mutex> guard(mutex_);
-    roll_ = euler[0];
-    pitch_ = euler[1];
+    roll_ = msg->data;
+  }
+
+  void pitch_callback(const std_msgs::msg::Float64::SharedPtr msg)
+  {
+    std::lock_guard<std::mutex> guard(mutex_);
+    pitch_ = msg->data;
   }
 
   double control_, roll_, pitch_;
@@ -71,7 +72,8 @@ private:
   double mass_;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr control_sub_;
-  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr roll_sub_;
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr pitch_sub_;
   rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_;
   std::mutex mutex_;
 };
